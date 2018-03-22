2be61f255139f2c8d540f97cfcac87126f1afe4b
renyi1314 <541421397@qq.com>
2018-03-22 23:32:12 +0800

    generator-producer-consumer

---
+++ Generator1.py
@@ -0,0 +1,19 @@
+def consumer():
+    last = ''
+    while True:
+        receival = yield last
+        if receival is not None:
+            print('Consume %s' % receival)
+            last = receival
+
+def producer(gen, n):
+    gen.__next__()
+    x = 0
+    while x < n:
+        x += 1
+        print('Produce %s' % x)
+        last = gen.send(x)
+        # print()
+    gen.close()
+gen = consumer()
+producer(gen, 5)
