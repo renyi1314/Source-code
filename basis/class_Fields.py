3186eff27c486caba99883d19860b18c115c057e
renyi1314 <541421397@qq.com>
2018-03-27 20:52:31 +0800

    init

---
+++ basis/class_Fields.py
@@ -0,0 +1,9 @@
+class foo():
+    def __init__(self,name):
+        self.name=name
+
+    age=18
+
+f=foo("nick")
+print(foo.age)
+print(f.name)
---
+++ basis/class_func.py
@@ -0,0 +1,16 @@
+class foo():
+    def __init__(self,name):
+        self.name=name
+
+    def f1(self):
+        print("normal f1")
+
+    @staticmethod
+    def f2():
+        print("staticmethod f2")
+
+    @classmethod
+    def f3(cls):
+        print("classmethod f3")
+
+foo.f3()
