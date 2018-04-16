a04cd378a34959fc995cda89daa28f77b2373283
renyi1314 <541421397@qq.com>
2018-04-16 14:30:49 +0800

    re

--- basis/re_test/re1.py
+++ basis/re_test/re1.py
@@ -1,15 +1,18 @@
-import re
-
-num = "13391931202"
-
-
-def is_phone_num(num):
-    match_mub = re.findall("^1[34578]\d{9}$", num)
-    if match_mub:
-        return True
-    else:
-        return False
-
-# def is_email
-
-is_phone_num(num)
+# import re
+#
+# num = "13391931202"
+#
+#
+#
+# re.findall("^1[34578]\d{9}$", num)
+#
+# re.findall("^[\\u4e00-\\u9fa5]{2,4}$", name):
+#
+# re.findall("^1[34578]\d{9}$", phone):
+#
+# re.findall("^\d{6,11}$", QQ):
+#
+# re.findall("^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$", email):
+#
+#
+# is_phone_num(num)
