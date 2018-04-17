4d2f17d0a3bb026c0b348d3ae3af18a27a60497d
renyi1314 <541421397@qq.com>
2018-04-17 11:01:06 +0800

    123

--- plus/decorator/decorator_single_object.py
+++ plus/decorator/decorator_single_object.py
@@ -1,18 +1,52 @@
+from functools import wraps, partial
+
+
+def update_wrapperr(wrapper,wrapped):
+    for attr in dir(wrapped):
+        try:
+            value = getattr(wrapped, attr)
+        except AttributeError:
+            pass
+        else:
+            # try:
+            setattr(wrapper, attr, value)
+            # except TypeError as e:
+            #     print(e)
+        finally:
+            setattr(wrapper, "__qualname__", "renyi")
+    for attr in ('__dict__',):
+        getattr(wrapper, attr).update(getattr(wrapped, attr, {}))
+    # Issue #17482: set __wrapped__ last so we don't inadvertently copy it
+    # from the wrapped function when updating __dict__
+    wrapper.__wrapped__ = wrapped
+    # Return the wrapper so this can be used as a decorator via partial()
+    return wrapper
+
+
+def decora(wrapped):
+    return partial(update_wrapperr, wrapped=wrapped)
+
+
+def fn():
+    pass
+
+
 def decorator_single_obj(cls, *args, **kwargs):
     instance = {}  # 创建字典来保存实例
+    # s = fn
+    # print(cls.__module__)
 
+    @decora(cls)
     def get_instance(*args, **kwargs):
         if cls not in instance:  # 若实例不存在则新建
             instance[cls] = cls(*args, **kwargs)
         return instance[cls]
-    print("-----------------")
-    print(instance)
-    print("*****************")
+
     return get_instance
 
 
 @decorator_single_obj
-class Foo(object):
+class Foo:
     age = 24
 
     def __init__(self, name):
@@ -24,11 +58,26 @@ class Foo(object):
 
 
 obj1 = Foo("123")
-print(obj1.age)
-print(dir(obj1))
-# print(Foo.age)
+print(Foo)
+print(dir(Foo))
+# print(Foo.upper())
 # print(dir(Foo))
+# print(id(obj1))
+# obj2 = Foo("321")
+# print(id(obj2))
+# obj1 = Foo("123")
+# print(Foo)
+# Foo.age
+# Foo.test
+# print(dir(Foo))
+# print(Foo.__wrapped__)
+
+# ('__module__', '__name__', '__qualname__', '__doc__',
+#                        '__annotations__')
+# print(dir(Foo))
+# print(obj1.age)
 # print(Foo.age)
-# print(obj1.name, id(obj1))
-obj2 = Foo("321")
-# print(obj2.name, id(obj2))
+# print(Foo)
+# obj2 = Foo("321")
+# print(Foo.test)
+# print(Foo.age)
