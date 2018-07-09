7fe041e63a88547811ae186bec3ac964d83fb853
renyi1314 <541421397@qq.com>
2018-07-09 20:48:07 +0800

    1

--- basis/data/test.py
+++ basis/data/test.py
@@ -1,3 +1,19 @@
-with open("data.txt",mode="wb") as f:
-    f.write("test")
-    f.seek()
+def singe_class(cls):
+    instance = {}
+
+    def wrap(*args, **kwargs):
+        if cls not in instance:
+            instance[cls] = cls(*args, **kwargs)
+        return instance[cls]
+
+    return wrap
+
+
+@singe_class
+class Foo:
+    pass
+
+
+a = Foo()
+b = Foo()
+print(id(a), id(b))
